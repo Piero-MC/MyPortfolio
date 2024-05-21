1987ce9076af1056a500c165108c52b0eb65e193
Piero <piero5005@outlook.es>
2024-05-21 16:00:26 -0500

    Inicio 21/05/2024

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import Mainp from "./Components/Mainp"
+import Navbar from "./Components/Navbar";
+import Main from "./Components/Mainp";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <Mainp />
+    </div>
+
+
+  );
+}
