23c7de69771f2eb435db7d123724d605fbb001fb
Piero <piero5005@outlook.es>
2024-06-10 13:42:30 -0500

    Projects section ended

---
+++ src/app/Components/Projectitem.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Image from "next/image";
+import Link from 'next/link';
+
+const Projectitem = ({title,backgroundImg,projectUrl}) => {
+    return (
+        <div className="relative flex items-center justify-center h-auto w-full shadow-xl shadow-gray-400 rounded-xl p-4 group hover:bg-gradient-to-r from-[#5651e5] to-[#709dff]">
+            <Image
+                className='rounded-xl group-hover:opacity-10'
+                src={backgroundImg}
+                alt='project'
+                width={400}
+                height={300}
+            />
+            <div className="hidden group-hover:block cursor-default absolute top-[50%] lef-[50%] translate-y-[-50%]">
+                <h3 class='text-2xl text-white tracking-wider text-center'>{title}</h3>
+                <p className='pb-4 pt-2 text-white text-center'>React JS</p>
+                <Link href={projectUrl}>
+                    <p className='text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Projectitem
--- src/app/Components/Projects.jsx
+++ src/app/Components/Projects.jsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import Image from "next/image";
+import Link from 'next/link';
+import Projectitem from './Projectitem';
 
 const Projects = () => {
+  const projectArr = [
+    {
+      title: 'Property Finder',
+      backgroundImg: '/about.jpg',
+      projectUrl: '/'
+    },
+    {
+      title: 'Property Finder',
+      backgroundImg: '/about.jpg',
+      projectUrl: '/'
+    },
+    {
+      title: 'Property Finder',
+      backgroundImg: '/about.jpg',
+      projectUrl: '/'
+    },
+    {
+      title: 'Property Finder',
+      backgroundImg: '/about.jpg',
+      projectUrl: '/'
+    }
+  ]
+
   return (
-    <div>Projects</div>
+    <div className="w-full h-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>Projects</p>
+        <h2 class='py-4'>What I've Build</h2>
+        <div className="grid md:grid-cols-2 gap-8">
+
+          {
+            projectArr.map(({ title, backgroundImg, projectUrl }) => (
+              <Projectitem
+                title={title}
+                backgroundImg={backgroundImg}
+                projectUrl={projectUrl} />
+            )
+            )
+          }
+
+        </div>
+      </div>
+    </div>
   )
 }
 
